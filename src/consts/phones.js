17d3ae08081632270f1c87f48ca904f3bcdacb23
DanQuan1 <danieltulip64@gmail.com>
2021-11-15 14:17:34 +0000

    Commit 2

--- src/consts/phones.js
+++ src/consts/phones.js
@@ -6,7 +6,7 @@ const phones = [
     like: true,
     img: require('../assets/12.jpg'),
     about:
-      'Succulent Plant is one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.',
+      'is a smartphone designed, developed, and marketed by Apple Inc. It is the fourteenth-generation, lower-priced iPhones, succeeding the iPhone 11. It was unveiled at a virtually held Apple Special Event at Apple Park in Cupertino, California on October 13, 2020, alongside the higher-end iPhone 12 Pro and iPhone 12 Pro Max flagships. Pre-orders for the iPhone 12 started on October 16, 2020, and the phone became available in most countries on October 23, 2020. Pre-orders for the iPhone 12 Mini began on November 6, 2020, and the phone became available on November 13, 2020.',
   },
 
   {
@@ -16,7 +16,7 @@ const phones = [
     like: false,
     img: require('../assets/12ProMax.jpg'),
     about:
-      'Dragon Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.',
+      'is a smartphone designed, developed, and marketed by Apple Inc. They are the fourteenth-generation, lower-priced iPhones, succeeding the iPhone 11. They were unveiled at a virtually held Apple Special Event at Apple Park in Cupertino, California on October 13, 2020, alongside the higher-end iPhone 12 Pro and iPhone 12 Pro Max flagships. Pre-orders for the iPhone 12 started on October 16, 2020, and the phone became available in most countries on October 23, 2020. Pre-orders for the iPhone 12 Mini began on November 6, 2020, and the phone became available on November 13, 2020.',
   },
 
   {
@@ -26,7 +26,7 @@ const phones = [
     like: false,
     img: require('../assets/13Pro.jpg'),
     about:
-      'Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.',
+    'The iPhone 13 Pro and iPhone 13 Pro Max are smartphones designed and marketed by Apple Inc. They are the flagship smartphones in the fifteenth generation of the iPhone, succeeding the iPhone 12 Pro and iPhone 12 Pro Max. They were announced on September 14, 2021, and became available ten days later, on September 24, 2021. The devices were unveiled alongside the iPhone 13 and iPhone 13 Mini at an Apple Special Event at Apple Park in Cupertino, California on September 14, 2021.',
   },
 
   {
@@ -35,8 +35,8 @@ const phones = [
     price: '5500',
     like: true,
     img: require('../assets/13ProMax.jpg'),
-    about:
-      'Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.',
+    about: 
+    'The iPhone 13 Pro and iPhone 13 Pro Max are smartphones designed and marketed by Apple Inc. They are the flagship smartphones in the fifteenth generation of the iPhone, succeeding the iPhone 12 Pro and iPhone 12 Pro Max. They were announced on September 14, 2021, and became available ten days later, on September 24, 2021. The devices were unveiled alongside the iPhone 13 and iPhone 13 Mini at an Apple Special Event at Apple Park in Cupertino, California on September 14, 2021.',
   },
 
   {
@@ -46,7 +46,7 @@ const phones = [
     like: true,
     img: require('../assets/Pixel6.jpg'),
     about:
-      'Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.',
+      'The Pixel 6 and Pixel 6 Pro are a pair of Android smartphones designed, developed, and marketed by Google as part of the Google Pixel product line.',
   },
 
   {
@@ -56,7 +56,7 @@ const phones = [
     like: false,
     img: require('../assets/P40Pro.jpg'),
     about:
-      'Potted Plant Ravenea Plant one of the most popular and beautiful species that will produce clumpms. The storage of water often gives succulent plants a more swollen or fleshy appearance than other plants, a characteristic known as succulence.',
+      'Huawei P40 is a line of high-end Android-based smartphones manufactured by Huawei.[1][2] Unveiled on 26 March 2020, they succeed the Huawei P30 in the company.',
   },
 
   {
@@ -66,7 +66,7 @@ const phones = [
     like: false,
     img: require('../assets/MotoZoom.jpg'),
     about: 
-      ' '
+      'the moto zoom is a smartphone that was recently released and it one of the powerful phones you will need.',
   },
 
   {
@@ -76,7 +76,7 @@ const phones = [
     like: false,
     img: require('../assets/MotoZ4.jpg'),
     about: 
-      ' '
+      'the Moto Z4 is a smartphone that was recently released and it one of the powerful phones you will need.',
   },
 
   {
@@ -85,7 +85,7 @@ const phones = [
     like: false,
     img: require('../assets/GalaxyS21Ultra.jpg'),
     about: 
-      ' '
+      'The Samsung Galaxy S21 Ultra is a series of Android-based smartphones designed, developed, marketed, and manufactured by Samsung Electronics as part of its Galaxy S series.',
   },
 
   {
@@ -94,7 +94,7 @@ const phones = [
     like: false,
     img: require('../assets/ZFlip3.jpg'),
     about: 
-      ' '
+      'The Samsung Galaxy Z Flip 3 (stylized as Samsung Galaxy Z Flip3) is a foldable smartphone that is part of the Samsung Galaxy Z series. It was revealed by Samsung Electronics on August 11, 2021 at the Samsung Unpacked event alongside the Z Fold 3. It is the successor to the Samsung Galaxy Z Flip. Although it is the second iteration of the Z Flip, it is branded as the Z Flip 3 for consistency with the Z Fold 3.',
   }
 ];
 
